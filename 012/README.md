76ed82132b1cc27cb69277b3731b73edfaf2fedf
xmdi <xmdi@protonmail.com>
2021-11-19 17:19:33 -0500

    video 12 code

---
+++ 012/README.md
@@ -0,0 +1,1 @@
+[VIDEO](https://youtu.be/QDV0Yi-FPHU)
---
+++ 012/main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <math.h>
+#include "geom.h"
+#include "trig.h"
+#include "draw.h"
+#include "out.h"
+
+int main(){
+
+	struct BODY* cube=makeRectangularPrism(-.5f,-.5f,-.5f,1.0f,1.0f,1.0f);
+	triangulateBody(cube);
+
+	float lookAt[3]={0,0,0};
+	float lookFrom[3]={1,1,1};
+
+	struct DRAWSTYLE* drawStyle=defaultDrawstyle();
+	drawBody(1000,1000,lookAt,lookFrom,cube,drawStyle);
+	
+	return 0;
+}
